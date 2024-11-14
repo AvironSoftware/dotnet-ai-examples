39f7c8f9288604bf6b898bf801ed9a22de8dbce5
Spencer Schneidenbach <spencer@avironsoftware.com>
2024-11-13 22:40:23 -0600

    add examples

---
+++ AzureOpenAITest/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System.ClientModel;
+using Azure.AI.OpenAI;
+using OpenAI.Chat;
+
+Console.WriteLine("Welcome to the Azure OpenAI Test!");
+
+var deploymentName = "";
+var endpointUrl = "";
+var key = "";
+
+var client = new AzureOpenAIClient(
+    new Uri(endpointUrl),
+    new ApiKeyCredential(key)
+);
+var chatClient = client.GetChatClient(deploymentName);
+
+var messages = new List<ChatMessage>
+{
+    new SystemChatMessage("You have a Southern accent and are friendly!")
+};
+
+while (true)
+{
+    Console.WriteLine("Say something to Azure OpenAI please!");
+    var line = Console.ReadLine();
+
+    if (line == "exit")
+    {
+        break;
+    }
+
+    messages.Add(new UserChatMessage(line));
+
+    var response = await chatClient.CompleteChatAsync(messages);
+    var chatResponse = response.Value.Content.Last().Text;
+
+    Console.WriteLine(chatResponse);
+    messages.Add(new AssistantChatMessage(chatResponse));
+}
---
+++ OpenAITest/Program.cs
@@ -0,0 +1,29 @@
+﻿using OpenAI.Chat;
+
+var openAIApiKey = "";
+var client = new ChatClient("gpt-4o", openAIApiKey);
+
+var messages = new List<ChatMessage>
+{
+    new SystemChatMessage("You have a Southern accent and are friendly!")
+};
+
+Console.WriteLine("Say something to OpenAI!");
+
+while (true)
+{
+    var line = Console.ReadLine();
+    if (line == "exit")
+    {
+        break;
+    }
+
+    messages.Add(new UserChatMessage(line));
+
+    var response = await client.CompleteChatAsync(messages);
+    var chatResponse = response.Value.Content.Last().Text;
+
+    Console.WriteLine(chatResponse);
+
+    messages.Add(new AssistantChatMessage(chatResponse));
+}
